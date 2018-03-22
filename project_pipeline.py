a29c9a1e38b0492d4a3e276ddc7e2ef7f39c22dc
gilboab <30528256+gilboab@users.noreply.github.com>
2018-03-22 01:58:45 -0500

    Update project_pipeline.py

--- project_pipeline.py
+++ project_pipeline.py
@@ -58,13 +58,13 @@ NUM_CLASSES = 90
 
 # In[ ]:
 
-#opener = urllib.request.URLopener()
-#opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-#tar_file = tarfile.open(MODEL_FILE)
-#for file in tar_file.getmembers():
-#  file_name = os.path.basename(file.name)
-#  if 'frozen_inference_graph.pb' in file_name:
-#    tar_file.extract(file, os.getcwd())
+opener = urllib.request.URLopener()
+opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+tar_file = tarfile.open(MODEL_FILE)
+for file in tar_file.getmembers():
+  file_name = os.path.basename(file.name)
+  if 'frozen_inference_graph.pb' in file_name:
+    tar_file.extract(file, os.getcwd())
 
 # ## Load a (frozen) Tensorflow model into memory.
 
